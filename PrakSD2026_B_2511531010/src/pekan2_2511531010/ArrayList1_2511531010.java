81bac7a46936f4076c30bd2049228d5f89f9dffe
MuhammadFharel <mfharell2222@gmail.com>
2026-04-18 15:50:53 +0700

    Update ArrayList1_2511531010.java

--- PrakSD2026_B_2511531010/src/pekan2_2511531010/ArrayList1_2511531010.java
+++ PrakSD2026_B_2511531010/src/pekan2_2511531010/ArrayList1_2511531010.java
@@ -1,9 +1,27 @@
 package pekan2_2511531010;
 
+import java.util.*;
+
 public class ArrayList1_2511531010 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//size of the ArrayList
+		int n = 5;
+		//Declaring the ArrayList with initial size n
+		ArrayList<Integer> arrli = new ArrayList<Integer> (n);
+		// Appending new elements at the end of the list
+		for (int i = 1; i <= n; i++)
+			arrli.add(i);
+		//Printing elements
+		System.out.println(arrli);
+		// Remove element at index 3
+		arrli.remove(3);
+		// Displaying the Arraylist
+		// after deletion
+		System.out.println(arrli);
+		// Printing elements one by one
+		for (int i = 0; i < arrli.size(); i++)
+			System.out.print(arrli.get(i) + " ");
 
 	}
 
