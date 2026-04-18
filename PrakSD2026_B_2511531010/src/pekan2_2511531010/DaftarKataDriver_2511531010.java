34387b92307d2a70786817a4650bb2f3a157ae9d
MuhammadFharel <mfharell2222@gmail.com>
2026-04-18 15:53:05 +0700

    Update DaftarKataDriver_2511531010.java

--- PrakSD2026_B_2511531010/src/pekan2_2511531010/DaftarKataDriver_2511531010.java
+++ PrakSD2026_B_2511531010/src/pekan2_2511531010/DaftarKataDriver_2511531010.java
@@ -1,5 +1,31 @@
 package pekan2_2511531010;
 
 public class DaftarKataDriver_2511531010 {
+	 public static void main(String[] args) {
+		 DaftarKata_2511531010 al = new DaftarKata_2511531010();
 
+	      // Menambah elemen (akhir)
+	      al.tambah_2511531010("Kami");
+	      al.tambah_2511531010("Informatika");
+
+	      // Menyisipkan elemen pada indeks 1
+	      al.tambahPada_2511531010(1, "Mahasiswa");
+
+	      // Cetak isi awal
+	      System.out.println("Awal           : " + al);
+
+	      // Mengubah elemen (index 1)
+	      al.ubahElemen_2511531010(1, "Departemen");
+	      System.out.println("Setelah Ubah: " + al);
+
+	      // Menghapus elemen (hapus index 0)
+	      String terhapus = al.hapusElement_2511531010(0);
+	      System.out.println("Terhapus       : " + terhapus);
+	      System.out.println("Setelah Hapus: " + al);
+
+	      // Iterasi pada ArrayList (cetak setiap elemen)
+	      System.out.print("Iterasi:");
+	      al.iterasiCetak();
+	      System.out.println();
+	  }
 }
